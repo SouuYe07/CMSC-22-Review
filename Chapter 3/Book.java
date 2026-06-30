9cce20606ce45f288642f5140a03a32627704f08
SouuYe07 <iesouaguila07@gmail.com>
2026-06-30 23:15:47 +0800

    Chapter 3

---
+++ Chapter 3/Book.java
@@ -0,0 +1,10 @@
+public class Book {
+    private Author author;
+    private String title;
+    private int noOfPages;
+    private boolean fiction;
+
+    public getAuthor(){
+        return this.author;
+    }
+}
